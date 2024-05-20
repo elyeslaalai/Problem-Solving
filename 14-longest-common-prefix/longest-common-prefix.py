c03adfe15cb8b090e3d6fe06f4f03ff30b8c7f17
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-05-20 06:47:11 +0200

    Time: 12 ms (85.52%) | Memory: 12 MB (29.62%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -6,18 +6,14 @@ class Solution(object):
         """
 
         res = []
-        shortest_word = strs[0]
+        strs = sorted(strs)
+        first, last = strs[0], strs[-1]
 
-        for s in strs:
-            if len(s) < len(shortest_word):
-                shortest_word = s
-        
-        for i in range(len(shortest_word)):
-            char = shortest_word[i]
-            for s in strs:
-                if s[i] != char:
-                    return ''.join(res)
-            res.append(char)
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return ''.join(res)
+            else:
+                res.append(first[i])
         
         return ''.join(res)
 
