f135538250e5d815345b7360223187c5ab4f0395
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-05-27 17:10:57 +0200

    Time: 11 ms (76.32%) | Memory: 11.6 MB (72.75%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,21 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        memo = {}
-        def climb(n):
-            if n == 0 or n == 1:
-                return 1
-            if n - 1 in memo:
-                first_poss = memo[n - 1]
-            else:
-                first_poss = climb(n - 1)
-                memo[n - 1] = first_poss
-            if n - 2 in memo:
-                second_poss = memo[n - 2]
-            else:
-                second_poss = climb(n - 2)
-                memo[n - 2] = second_poss
-            return first_poss + second_poss
-        return climb(n)
-
-        
+        if n == 0 or n == 1:
+            return 1
+        prev, curr = 1, 1
+        for i in range(2, n + 1, 1):
+            temp = curr
+            curr = prev + curr
+            prev = temp
+        return curr
