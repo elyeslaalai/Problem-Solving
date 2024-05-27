c1616bb3eedf5231869a39d14707e23179d732e7
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-05-27 15:28:25 -0400

    Time: 31 ms (55.69%) | Memory: 16.9 MB (41.43%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -11,23 +11,13 @@ class Solution(object):
         :rtype: bool
         """
         def depth(root):
-            if not root: 
+            if not root:
                 return 0
-            elif not root.left and not root.right:
-                return 1
-            elif not root.left:
-                return 1 + depth(root.right)
-            elif not root.right:
-                return 1 + depth(root.left)
-            else:
-                return 1 + max(depth(root.right), depth(root.left))
+            return 1 + max(depth(root.left), depth(root.right))
         if not root:
             return True
-        left_depth = 0 if not root.left else depth(root.left)
-        right_depth = 0 if not root.right else depth(root.right)
-        curr_check = abs(left_depth - right_depth) <= 1
-        left_check = self.isBalanced(root.left)
-        right_check = self.isBalanced(root.right)
-        return curr_check and left_check and right_check
+        left = depth(root.left)
+        right = depth(root.right)
+        return abs(left - right) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
             
 
