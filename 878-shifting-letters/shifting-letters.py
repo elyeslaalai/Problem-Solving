b9053c366a454647be5df2291c9cf81488172a97
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-06-13 21:27:47 -0400

    Time: 616 ms (77.50%) | Memory: 23.6 MB (7.50%) - LeetSync

--- 878-shifting-letters/shifting-letters.py
+++ 878-shifting-letters/shifting-letters.py
@@ -22,18 +22,8 @@ class Solution(object):
 
             char = s[i]
             shift = total_shifts[i]
-
             list_s[i] = chr( ord('a') + (ord(char) - ord('a') + shift) % 26 )
-            print(ord('a'))
-            print(ord('z'))
-            print(ord('a') + (ord(char) + shift) % ord('z'))
         
-    
-        # print(ord('z'))
-        # print(ord('z') + 3)
-        # print(chr(ord('a') - 1 + (ord('z') + 3) % ord('z')))
-        # print(ord('c'))
-        # print(ord('z'))
         return "".join(list_s)
 
 
