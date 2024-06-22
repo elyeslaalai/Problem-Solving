510f3a49e1a1ca4f97df7ecf08aaf25bffde89e3
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-06-22 15:26:30 -0400

    Time: 86 ms (75.65%) | Memory: 12.7 MB (6.96%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,18 +5,45 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
 
-        hset_rows = set()
-        hset_cols = set()
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if matrix[r][c] == 0:
-                    hset_rows.add(r)
-                    hset_cols.add(c)
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
-                if r in hset_rows or c in hset_cols:
-                    matrix[r][c] = 0
-        return matrix
+        num_rows = len(matrix)
+        num_cols = len(matrix[0])
 
+        first_row_zero = False
+        first_col_zero = False
 
+        # Checks the first row
+        for col in range(num_cols):
+            if matrix[0][col] == 0:
+                first_row_zero = True
+                break
+        
+        # Checks the first col
+        for row in range(num_rows):
+            if matrix[row][0] == 0:
+                first_col_zero = True
+                break
+        
+        # Checks the rest
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if matrix[row][col] == 0:
+                    matrix[row][0] = 0
+                    matrix[0][col] = 0
+
+        # Replaces the inside
+        for row in range(1, num_rows):
+            for col in range(1, num_cols):
+                if matrix[row][0] == 0 or matrix[0][col] == 0:
+                    matrix[row][col] = 0
+
+        # Replaces the boundaries
+        if first_row_zero:
+            for col in range(num_cols):
+                matrix[0][col] = 0
+        if first_col_zero:
+            for row in range(num_rows):
+                matrix[row][0] = 0
+
+        return matrix        
+                
         
