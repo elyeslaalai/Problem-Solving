8ab1568b641a1f253503cbfb8f4d70fffe34e261
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-06-22 16:49:22 -0400

    Time: 39 ms (39.38%) | Memory: 12.1 MB (99.35%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+
+            while l < len(s) and not s[l].isalnum():
+                l += 1
+            
+            while r >= 0 and not s[r].isalnum():
+                r -= 1
+            
+            if l >= r:
+                return True
+
+            if s[l].upper() != s[r].upper():
+                return False
+            
+            l += 1
+            r -= 1
+        
+        return True
+        
