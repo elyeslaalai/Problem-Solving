cc6060048c6673cd783bdc1021da4d061be1f743
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-08-16 11:13:25 -0400

    Time: 39 ms (63.31%) | Memory: 11.5 MB (64.87%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,20 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-
         if x < 0:
             return False
         
-        elif x != 0 and x % 10 == 0:
-            return False
-        
         else:
-            reverse_num = 0
-            copy_x = x
-            while x != 0:
-                reverse_num = reverse_num * 10 + x % 10
-                x /= 10
-            return reverse_num == copy_x
-
-
-
+            flipped_x = str(x)[::-1]
+            return flipped_x == str(x)
+        
