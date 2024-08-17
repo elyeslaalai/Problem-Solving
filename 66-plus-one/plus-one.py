63366d89425291e04eb6f54d546c9fc877f8aebb
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-08-17 10:38:07 -0400

    Time: 18 ms (38.87%) | Memory: 11.6 MB (31.38%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -5,16 +5,9 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        if digits[-1] != 9:
-            digits[-1] += 1
-            return digits
-
-
-        for i in range(len(digits) - 1, -1, -1):
+        for i in range(len(digits)-1,-1,-1):
             if digits[i] != 9:
                 digits[i] += 1
                 return digits
-            else:
-                digits[i] = 0
-        
+            digits[i] = 0
         return [1] + digits
