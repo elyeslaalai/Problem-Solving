d2068f0613f5eeec4a0352f57c5fb6a1c8ece979
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-08-17 10:08:00 -0400

    Time: 8 ms (90.61%) | Memory: 11.9 MB (19.13%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -4,11 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        
         i = len(s) - 1
-        while s[i] == ' ':
+
+        while i >= 0 and not s[i].isalnum():
             i -= 1
-        ans = 0
+        
+        res = 0
+        
         while i >= 0 and s[i] != ' ':
-            ans += 1
-            i -= 1
-        return ans
+            i -=1
+            res +=1 
+        
+
+        return res
