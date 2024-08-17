5179808794ea7b7c2fcc93f8af50e4eeaf0c3b9e
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-08-17 10:36:27 -0400

    Time: 15 ms (61.77%) | Memory: 11.6 MB (31.38%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,11 +4,17 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
+        
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+
+
         for i in range(len(digits) - 1, -1, -1):
-            if digits[i] == 9:
-                digits[i] = 0
-            else:
+            if digits[i] != 9:
                 digits[i] += 1
                 return digits
-        return [1] + digits
+            else:
+                digits[i] = 0
         
+        return digits if digits[0] else [1] + digits
