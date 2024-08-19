ff2bd6550723003416bb6c28a019e847e7762d02
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-08-19 05:41:28 -0400

    Time: 7 ms (98.55%) | Memory: 11.8 MB (7.04%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        l, r = 0, x
+
+        while l <= r:
+            mid = (l + r) / 2
+            if mid * mid <= x < (mid + 1) * (mid + 1):
+                return mid
+            elif mid * mid > x:
+                r = mid - 1
+            elif (mid + 1) * (mid + 1) <= x:
+                l = mid + 1
+        
