569eae5c42f7450891b540da38b6f1de0c4f65a4
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-09-01 15:51:19 -0400

    Time: 19 ms (14.54%) | Memory: 11.6 MB (35.24%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/excel-sheet-column-title.py
@@ -9,11 +9,13 @@ class Solution(object):
 
         while columnNumber:
             
-            offset = (columnNumber - 1) % 26
+            columnNumber -= 1
+
+            offset = columnNumber % 26
             
             result.append(chr(ord('A') + offset))
 
-            columnNumber = (columnNumber - 1) / 26
+            columnNumber /= 26
             
 
         return "".join(result[::-1])
