9bbd8abd60677fd296f71a6fb00ffbc12465d70d
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-09-01 15:53:00 -0400

    Time: 163 ms (48.05%) | Memory: 13.4 MB (40.02%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,21 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maj_element = nums[0]
-        maj_occurences = 1
 
-        for i in range(1, len(nums)):
-
-            num = nums[i]
-
-            if num == maj_element:
-                maj_occurences += 1
-            
-            else:
-                maj_occurences -= 1
-                
-                if maj_occurences == 0:
-                    maj_occurences = 1
-                    maj_element = num
-
-        return maj_element
+        hmap = {}
+        for num in nums:
+            hmap[num] = hmap.get(num, 0) + 1
+            if hmap[num] > len(nums) / 2:
+                return num
+        
