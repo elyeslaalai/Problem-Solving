1a3451541fcd4219c9ba2bd1f405efb513f4832a
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-09-02 11:06:16 -0400

    Time: 46 ms (91.47%) | Memory: 27.3 MB (60.87%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -1,3 +1,4 @@
+import math
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -10,16 +11,18 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-
+        def leftHeight(root):
+            if not root:
+                return 0
+            return 1 + leftHeight(root.left)
+        def rightHeight(root):
+            if not root:
+                return 0
+            return 1 + rightHeight(root.right)
         if not root:
             return 0
-        
-        elif not root.left:
-            return 1 + self.countNodes(root.right)
-        
-        elif not root.right:
-            return 1 + self.countNodes(root.left)
-        
-        else:
-            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
-        
+        leftHeight = leftHeight(root)
+        rightHeight = rightHeight(root)
+        if leftHeight == rightHeight:
+            return int(math.pow(2, leftHeight)) - 1
+        return self.countNodes(root.left) + self.countNodes(root.right) + 1
