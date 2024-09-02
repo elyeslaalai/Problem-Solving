83f3a944b51e5cd1e8071ebfa5857ca81bcdf39f
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-09-02 10:54:24 -0400

    Time: 17 ms (22.43%) | Memory: 11.6 MB (85.91%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -6,32 +6,23 @@ class Solution(object):
         """
 
         if not nums:
-            return ""
+            return []
         
         if len(nums) == 1:
-            num = nums[0]
-            print("XXXX")
-            return [str(num)]
+            return [str(nums[0])]
         
         result = []
         first, last = nums[0], nums[0]
 
-        for i in range(1, len(nums)):
-            num = nums[i]
-            if num - last == 1:
-                last = num
-            else:
+        for i in range(1, len(nums) + 1):
+            if i == len(nums) or nums[i] - last != 1:
                 if first == last:
                     result.append(str(first))
                 else:
                     result.append(str(first) + "->" + str(last))
-                first = num
-                last = num
-        if first == last:
-            result.append(str(first))
-        else:
-            result.append(str(first) + "->" + str(last))
+                if i != len(nums):
+                    first, last = nums[i], nums[i]
+            else:
+                last = nums[i]
+
         return result
-
-
-        
