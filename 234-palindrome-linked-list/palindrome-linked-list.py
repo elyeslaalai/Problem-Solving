fe04c2696fd6403123b12b15782f9c4f45936f79
Elyes Laalai <117252462+elyeslaalai@users.noreply.github.com>
2024-09-02 12:33:05 -0400

    Time: 746 ms (55.23%) | Memory: 84.3 MB (10.36%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -15,34 +15,29 @@ class Solution(object):
         if not head or not head.next:
             return True
         
-        temp = head
         length = 0
-
+        temp = head
         while temp:
             length += 1
             temp = temp.next
-
+        
         temp = head
         stack = []
+
         for _ in range(length / 2):
             stack.append(temp.val)
             temp = temp.next
         
-
         start = length / 2
-        if length % 2 != 0:
+        if length % 2 == 1:
             temp = temp.next
             start += 1
         
-        print(start)
-        
         for _ in range(start, length):
             popped = stack.pop()
-            print(popped)
-            print(temp.val)
-            if temp.val != popped:
+            if popped != temp.val:
                 return False
             temp = temp.next
-        
+
         return True
-        
+    
